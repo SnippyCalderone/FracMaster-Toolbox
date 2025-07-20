6feae677c5f498b8a1ec3892979eb884e5c2e957
SnippyCalderone <zwp5033@gmail.com>
2025-07-20 10:53:44 -0400

    Add module entry point

---
+++ fracmaster_toolbox/__main__.py
@@ -0,0 +1,10 @@
+from fracmaster_toolbox.gui.main import launch_main_window
+
+
+def main() -> None:
+    """Module entry point for ``python -m fracmaster_toolbox``."""
+    launch_main_window()
+
+
+if __name__ == "__main__":
+    main()
